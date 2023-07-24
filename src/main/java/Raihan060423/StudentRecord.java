fa8334a04eaaf252ee3c473f73cbc40afb386f6a
Raihannn21 <muhammadraihanpratama222@gmail.com>
2023-07-24 23:42:11 +0700

    RAIHAN060423

--- src/main/java/Raihan060423/StudentRecord.java
+++ src/main/java/Raihan060423/StudentRecord.java
@@ -9,111 +9,109 @@ package Raihan060423;
  * @author Acer
  */
 public class StudentRecord {
-    private String name; 
-    private String address; 
-    private int age; 
-    private double mathGrade; 
-    private double englishGrade; 
-    private double scienceGrade; 
+     private String name;
+    private String address;
+    private int age;
+    private double mathGrade;
+    private double englishGrade;
+    private double scienceGrade;
     private double average;
     private char nilaihuruf;
-    
     private static int studentCount; //class variabel
+    //static bisa lansung diakses tanpa instance
     
-    public StudentRecord(){ 
-    //area inisialisasi kode;
-    studentCount++;
-    } 
-    public StudentRecord(String temp){ 
-    this.name = temp;
-    studentCount++;
-    } 
-    public StudentRecord(String name, String address){ 
-    this.name = name; 
-    this.address = address;
-    studentCount++;
-    } 
-    public StudentRecord(double mGrade, double eGrade, double sGrade){ 
-    mathGrade = mGrade; 
-    englishGrade = eGrade; 
-    scienceGrade = sGrade;
-    studentCount++;
+    public StudentRecord(){
+        //area inisialisasi kode tanpa parameter
+        studentCount++;
+    }
+    public StudentRecord(String temp){
+        this.name = temp;
+        studentCount++;
+    }
+    public StudentRecord(String name, String address){
+        this.name = name;
+        this.address = address;
+        studentCount++;
+    }
+    public StudentRecord(double mGrade, double eGrade, double sGrade){
+        mathGrade = mGrade;
+        englishGrade = eGrade;
+        scienceGrade = sGrade;
+        studentCount++;
     }
     
-    public String getName(){ 
-        return name; 
-    } 
-    
-    public void setName( String name ){ 
-        this.name = name; 
+    //accessor method
+    public String getName(){
+        return name;
+    }
+    //mutator method
+    public void setName(String name){
+        this.name = name;
     }
     
-    public String getAddress(){ 
-        return address; 
-    } 
-    
-    public void setAddress( String address ){ 
-        this.address = address; 
+    public String getAddress(){
+        return address;
+    }
+    public void setAddress(String address){
+        this.address = address;
     }
     
-    public int getAge(){ 
-        return age; 
-    } 
-    
-    public void setAge( int age ){ 
-        this.age = age; 
+    public int getAge(){
+        return age;
+    }
+    public void setAge(int age){
+        this.age = age;
     }
     
-     public double getMathGrade(){ 
-        return mathGrade; 
-    } 
-    
-    public void setMathGrade( double mathGrade ){ 
-        this.mathGrade = mathGrade; 
+    public double getMathGrade(){
+        return mathGrade;
+    }
+    public void setMathGrade(double mathGrade){
+        this.mathGrade = mathGrade;
     }
     
-    public double getEnglishGrade(){ 
-        return englishGrade; 
-    } 
-    
-    public void setEnglishGrade( double englishGrade ){ 
-        this.englishGrade = englishGrade; 
+    public double getEnglishGrade(){
+        return englishGrade;
+    }
+    public void setEnglishGrade(double englishGrade){
+        this.englishGrade = englishGrade;
     }
     
-    public double getScienceGrade(){ 
-        return scienceGrade; 
-    } 
-    
-    public void setScienceGrade( double scienceGrade ){ 
-        this.scienceGrade = scienceGrade; 
+    public double getScienceGrade(){
+        return scienceGrade;
+    }
+    public void setScienceGrade(double scienceGrade){
+        this.scienceGrade = scienceGrade;
     }
     
     public double getAverage(){
-        average = (mathGrade + englishGrade + scienceGrade)/3;
-        return average; 
+        average =(getMathGrade() + getEnglishGrade() + getScienceGrade())/3;
+        return average;
     }
     
-    public static int getStudentCount(){ 
-        return studentCount; 
-    }  
-    
-    public void print( String temp ){ 
-        System.out.println("Name:" + name); 
-        System.out.println("Address:" + address); 
-        System.out.println("Age:" + age); 
+    public static int getStudentCount(){
+        return studentCount;
     }
-    public void print(double eGrade, double mGrade, double sGrade){ 
-        System.out.println("Name:" + name); 
-        System.out.println("Math Grade:" + mGrade);
-        mathGrade = mGrade;
-        System.out.println("English Grade:" + eGrade); 
+    
+    public void print( String temp ){
+        System.out.println("Name    : " + name);
+        System.out.println("Address : " + address);
+        System.out.println("Age     : " + age);
+    }
+    public void print(double eGrade, double mGrade, double sGrade){
+        System.out.println("Name         : " + name);
+        System.out.println("Math Grade   : " + mGrade);
+        mathGrade = mGrade; 
+        System.out.println("English Grade: " + eGrade);
         englishGrade = eGrade;
-        System.out.println("Science Grade:" + sGrade); 
+        System.out.println("Science Grade: " + sGrade);
         scienceGrade = sGrade;
-        System.out.println("average:" + getAverage());
-        System.out.println("nilaihuruf:" + nilaiHuruf());
-        System.out.println();
-    }
+        System.out.println("Avegare      : " + getAverage());
+        System.out.println("Nilai huruf  : " + nilaiHuruf());
+        
+    } 
+    
+    //tambahan 060423
     public char nilaiHuruf(){
         if(getAverage() > 80)
             nilaihuruf = 'A';
@@ -123,8 +121,8 @@ public class StudentRecord {
             nilaihuruf = 'C';
         else if(getAverage() > 40)
             nilaihuruf = 'D';
-         else if(getAverage() > 0)
-             nilaihuruf = 'E';
+        else if(getAverage() > 0)
+            nilaihuruf = 'E';
         return nilaihuruf;
     }
 }
