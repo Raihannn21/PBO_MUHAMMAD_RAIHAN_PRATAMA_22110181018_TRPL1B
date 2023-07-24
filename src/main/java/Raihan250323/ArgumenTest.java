5a30a96377baaf62b086925b3e79e2725bcae469
Raihannn21 <muhammadraihanpratama222@gmail.com>
2023-07-24 23:46:11 +0700

    RAIHAN 150423

---
+++ src/main/java/Raihan250323/ArgumenTest.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Raihan250323;
+
+/**
+ *
+ * @author Acer
+ */
+public class ArgumenTest {
+    public static void main(String[] args){
+        System.out.println("banyak argument : "+ args.length);
+        System.out.println("argument pertama: "+args[0]);
+    }
+}
